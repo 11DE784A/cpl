c506a3cbebbc1044f21e426df87298217e7f4485
Ayush Singh <ayush.singh@niser.ac.in>
2022-03-01 00:37:53 +0530

    include check to avoid including headers multiple times

--- cpl_defines.h
+++ cpl_defines.h
@@ -1,5 +1,10 @@
+#ifndef CPL_DEFINES
+
+#define CPL_DEFINES
+
 #define scalar double
 
 #define TOL 1e-6
 #define MAX_ITERS 20000
 
+#endif
--- cpl_includes.h
+++ cpl_includes.h
@@ -1,5 +1,15 @@
+#define _GNU_SOURCE
+
+#ifndef CPL_INCLUDES
+
+#define CPL_INCLUDES
+
+#include <dlfcn.h>
 #include <stdarg.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
+#endif
