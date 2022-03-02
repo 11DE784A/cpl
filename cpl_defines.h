447295f32244e28376414c45c3eec0815a18560e
Ayush Singh <ayush.singh@niser.ac.in>
2022-03-02 18:33:27 +0530

    macro for converting identifiers to strings and a s(calar)abs macro for absolute value of scalars

--- cpl_defines.h
+++ cpl_defines.h
@@ -2,9 +2,20 @@
 
 #define CPL_DEFINES
 
+#define xstr(x) str(x)
+#define str(x) #x
+
 #define scalar double
+
+#define sfmt % -#9.4g
 
 #define TOL 1e-6
 #define MAX_ITERS 20000
+
+#define sabs(x) _Generic((x), \
+	float: fabsf, \
+	double: fabs, \
+	default: abs \
+)(x)
 
 #endif
